dc45589722a7523ffcab5ba4d27eb00259c1090e
Tomas Vana <tomas@Tomas-Vanas-iMac.local>
2013-03-02 13:22:24 +0100

    Empty Project

---
+++ Tasks/IAAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IAAAppDelegate.h
+//  Tasks
+//
+//  Created by Tomas Vana on 3/2/13.
+//  Copyright (c) 2013 iOS Apps Austria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tasks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tasks
+//
+//  Created by Tomas Vana on 3/2/13.
+//  Copyright (c) 2013 iOS Apps Austria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IAAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IAAAppDelegate class]));
+    }
+}
