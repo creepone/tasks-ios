a13efb082c1af99f838b65b9eea7a893d79aab5d
Tomas Vana <creepone@gmail.com>
2014-04-27 18:03:40 +0200

    fixed build script

--- Tasks/prebuild.sh
+++ Tasks/prebuild.sh
@@ -1,4 +1,12 @@
+# make sure we fail on non-zero exit code of commands
+set -e
+set -o pipefail
+
 # install pod dependencies
+export PATH=$PATH:$HOME/.rvm/bin  # Add RVM to PATH for scripting
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+rvm use 2.1.1
+export LC_ALL="en_US.UTF-8"
 pod install
 
 #  make sure that the revision.prefix file is updated so that it contains the actual build number
